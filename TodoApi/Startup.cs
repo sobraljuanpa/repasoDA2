e68509403b2706b87269d7b58fc0d7e2f2826f2d
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-02-21 18:51:44 -0300

    Added sqlite support

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -29,7 +29,7 @@ namespace TodoApi
         {
 
             services.AddControllers();
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlite("Data Source=todos.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
