7ae33b3e353febb7e21cad91dcf612484e42fd27
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-02-21 17:59:20 -0300

    removed swagger injection

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -38,8 +38,6 @@ namespace TodoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
             }
 
             app.UseHttpsRedirection();
